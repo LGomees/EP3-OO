85033e70bf328a40aaa4e49181794e4f50c79297
Lucas Gomes <l.gomees_@hotmail.com>
2017-06-24 19:48:59 -0300

    [FILES] Translation icons and update routes

--- WikiSeries/config/routes.rb
+++ WikiSeries/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :users
   get 'home' => 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
